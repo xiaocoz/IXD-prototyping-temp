9cab06869a84f821b65ccf26bd477610894573a3
xiaocoz <137859417+xiaocoz@users.noreply.github.com>
2023-10-20 11:06:45 -0700

    Update main.py

--- Project_2/main.py
+++ Project_2/main.py
@@ -16,22 +16,22 @@ def draw():
   circle_size = int(data)
   p5.noStroke()
   p5.fill(150)
-  p5.ellipse(150, 150, circle_size, circle_size)
+  #p5.ellipse(150, 150, circle_size, circle_size)
   p5.push()
-  #set angle variable to integer of data
+  # set angle variable to integer of data
   angle = int(data)
-  #move the canvas to the center
-  p5.translate(p5.width/2,p5.height/2)
-  #rotate the canvas with angle converted from degrees to radians:
+  # move to middle of canvas
+  p5.translate(p5.width/2, p5.height/2)
+  # rotate canvas with angle converted from degrees to radians:
   p5.rotate(p5.radians(angle))
-  # Change mode to draw rectangles from the center:
+  # change mode to draw rectangles from center:
   p5.rectMode(p5.CENTER)
-  # draw rect at coordinate 0,0
-  p5.rect(0,0,100,100)
-  # restore graphical transformation:
+  # draw rectangle at coordinate 0, 0 and 100 width and height:
+  p5.rect(0, 0, 100, 100)
+  # rectore graphical transformation:
   p5.pop()
 
-def print_test(x):
-  print(x)
+
+
 
   
